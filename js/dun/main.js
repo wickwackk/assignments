1c506a5b8d91c26b64b7e2e355a0d41bffcc5ba1
Morning <morning@admins-iMac-3.local>
2022-11-29 13:33:17 +0800

    after class nov29

---
+++ js/dun/main.js
@@ -0,0 +1,71 @@
+let bat = 67,
+  ontsDumd = 59,
+  tulga = 80,
+  nomin = 95,
+  tugsgerel = 99,
+  onoo = 50;
+
+if (90 <= bat && bat <= 100) {
+  unelgee = "Mash sain";
+} else if (80 <= bat && bat < 90) {
+  unelgee = "Sain";
+} else if (70 <= bat && bat < 80) {
+  unelgee = "Dund";
+} else if (60 <= bat && bat < 70) {
+  unelgee = "Hangalttai";
+} else if (bat < 60) {
+  unelgee = "Mash muu";
+}
+console.log("Bat", bat, unelgee);
+
+if (90 <= ontsDumd && ontsDumd <= 100) {
+  unelgee = "Mash sain";
+} else if (80 <= ontsDumd && ontsDumd < 90) {
+  unelgee = "Sain";
+} else if (70 <= ontsDumd && ontsDumd < 80) {
+  unelgee = "Dund";
+} else if (60 <= ontsDumd && ontsDumd < 70) {
+  unelgee = "Hangalttai";
+} else if (ontsDumd < 60) {
+  unelgee = "Mash muu";
+}
+console.log("Onts Dumd", ontsDumd, unelgee);
+
+if (90 <= tulga && tulga <= 100) {
+  unelgee = "Mash sain";
+} else if (80 <= tulga && tulga < 90) {
+  unelgee = "Sain";
+} else if (70 <= tulga && tulga < 80) {
+  unelgee = "Dund";
+} else if (60 <= tulga && tulga < 70) {
+  unelgee = "Hangalttai";
+} else if (tulga < 60) {
+  unelgee = "Mash muu";
+}
+console.log("Tulga", tulga, unelgee);
+
+if (90 <= nomin && nomin <= 100) {
+  unelgee = "Mash sain";
+} else if (80 <= nomin && nomin < 90) {
+  unelgee = "Sain";
+} else if (70 <= nomin && nomin < 80) {
+  unelgee = "Dund";
+} else if (60 <= nomin && nomin < 70) {
+  unelgee = "Hangalttai";
+} else if (nomin < 60) {
+  unelgee = "Mash muu";
+}
+console.log("Nomin", nomin, unelgee);
+
+if (90 <= tugsgerel && tugsgerel <= 100) {
+  unelgee = "Mash sain";
+} else if (80 <= tugsgerel && tugsgerel < 90) {
+  unelgee = "Sain";
+} else if (70 <= tugsgerel && tugsgerel < 80) {
+  unelgee = "Dund";
+} else if (60 <= tugsgerel && tugsgerel < 70) {
+  unelgee = "Hangalttai";
+} else if (tugsgerel < 60) {
+  unelgee = "Mash muu";
+}
+console.log("Tugsgerel", tugsgerel, unelgee);
