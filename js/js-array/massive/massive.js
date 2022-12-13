b9e6b63d422a6e3a34e493b739e11193b5061d3c
Morning <morning@admins-iMac-3.local>
2022-12-13 12:22:40 +0800

    dec13 at class

--- js/js-array/massive/massive.js
+++ js/js-array/massive/massive.js
@@ -2,18 +2,17 @@ let arr1 = [3, 45, 23, 78, 34];
 let arr2 = [4, 2, 34, 4, 12, 1];
 let output = [];
 
-for (let i = 0; i < arr1.length; i++) {
+for (let i = 0; i < arr2.length; i++) {
   let temp = 0;
-  for (let j = 0; j < arr2.length; j++) {
-    if (i == j) {
-      temp = arr1[i] * arr2[j];
-      output.push(temp);
-    } else if (arr1[j] == undefined) {
-      output.push(arr[j]);
-    } else if (arr2[i] == undefined) {
-      temp = arr1[i];
-      output.push(temp);
-    }
+  temp = arr1[i] * arr2[i];
+  output.push(temp);
+  if (Number.isInteger(temp) == false) {
+    output.pop();
+  }
+  if (arr1[i] == undefined) {
+    output.push(arr2[i]);
+  } else if (arr2[i] == undefined) {
+    output.push(arr1[i]);
   }
 }
 console.log(output);
